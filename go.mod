d92aa5a70df1bbb495b764053de5166551766abd
oltdaniel <git@oltdaniel.eu>
2025-04-05 12:14:16 +0200

    polish dev script and fix minor issues

--- go.mod
+++ go.mod
@@ -2,8 +2,9 @@ module helloworld
 
 go 1.24.1
 
+require github.com/gofiber/template/html/v2 v2.1.3
+
 require (
 	github.com/gofiber/template v1.8.3 // indirect
-	github.com/gofiber/template/html/v2 v2.1.3 // indirect
 	github.com/gofiber/utils v1.1.0 // indirect
 )
--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,9 +1,20 @@
 import { debounce } from "jsr:@std/async/debounce";
+import { basename, relative, resolve, SEPARATOR } from "jsr:@std/path";
+
+function isPathInside(childPath: string, parentDir: string): boolean {
+  const resolvedChild = resolve(Deno.realPathSync(childPath));
+  const resolvedParent = resolve(Deno.realPathSync(parentDir));
+
+  const rel = relative(resolvedParent, resolvedChild);
+
+  // If the relative path does not start with `..`, it's inside
+  return !!rel && !rel.startsWith("..") && !rel.includes(`..${SEPARATOR}`);
+}
 
 // Run bash script in background for web build
 const runCmd = new Deno.Command(Deno.execPath(), {
   args: ["task", "run"],
-  cwd: `${import.meta.dirname}`,
+  cwd: `${import.meta.dirname}/..`,
 });
 
 console.log("🚀 Running application in background...");
@@ -21,10 +32,12 @@ function restartRunProc() {
   runProc = runCmd.spawn();
 }
 
+const debouncedRestart = debounce(restartRunProc, 1000);
+
 // Run bash script in background for web build
 const webBuildCmd = new Deno.Command(Deno.execPath(), {
   args: ["task", "dev"],
-  cwd: `${import.meta.dirname}/web`,
+  cwd: `${import.meta.dirname}/../web`,
 });
 
 console.log("🚀 Running web build in background...");
@@ -34,7 +47,7 @@ const webBuildProc = webBuildCmd.spawn();
 const watcher = Deno.watchFs(".", { recursive: true });
 
 // Debounce go build to avoid unnecessary rebuilds
-const debouncedGoBuild = debounce(async (_: Deno.FsEvent) => {
+const debouncedGoBuild = debounce(async (_?: Deno.FsEvent) => {
   console.log("🛠️ Change detected, running `build:go` task...");
   const goBuildCmd = new Deno.Command(Deno.execPath(), {
     args: ["task", "build:go"],
@@ -52,18 +65,32 @@ const debouncedGoBuild = debounce(async (_: Deno.FsEvent) => {
   }
 }, 200);
 
+debouncedGoBuild();
+
+// Register exit handler
+Deno.addSignalListener("SIGINT", () => watcher.close());
+
 // Start loop of handling file watcher events
 console.log("👀 Watching for Go files changes...");
 
 for await (const event of watcher) {
   if (event.kind === "modify" || event.kind === "create") {
-    const relevant = event.paths.some((path) =>
-      path.endsWith(".go") || path === "go.mod" || path === "go.sum"
+    const goBuildRelevant = event.paths.some((path) => {
+      const filename = basename(path);
+      return (filename.endsWith(".go") || filename === "go.mod" ||
+        filename === "go.sum") &&
+        (!isPathInside(path, `${import.meta.dirname}/../web/`));
+    });
+
+    const serverRestartRelevant = event.paths.some((path) =>
+      isPathInside(path, `${import.meta.dirname}/../web/dist`)
     );
 
-    if (relevant) {
+    if (goBuildRelevant) {
       // Call a debounced build to avoid too frequent builds
       debouncedGoBuild(event);
+    } else if (serverRestartRelevant) {
+      debouncedRestart();
     }
   }
 }
